3834aa6fe65f4837ed6cb9ec9d1f3a0edcd5a940
RTemler <rtemler10@gmail.com>
2025-04-06 15:37:41 -0400

    edit

--- My project (2)/Assets/Scripts/followPlayer.cs
+++ My project (2)/Assets/Scripts/followPlayer.cs
@@ -8,6 +8,6 @@ public class followPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + new Vector3(0, 1, -5);
+        transform.position = player.transform.position + new Vector3(0, 0.5f, -5);
     }
 }
